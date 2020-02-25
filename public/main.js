f331bdf740c3626ebe3bfb40c742fbace723e7e6
ChaseAnzelc <cjanzelc@gmail.com>
2020-02-25 11:38:30 -0600

    fixing bug for back to top button

--- public/main.js
+++ public/main.js
@@ -15,13 +15,6 @@ $(document).ready(function() {
   }
 });
 
-$('.back-to-top').click(function() {
-  $('html, body').animate({
-    scrollTop: 0
-  }, 1500, 'easeInOutExpo');
-  return false;
-});
-
 
   // ========================================================================= //
   //  //SMOOTH SCROLL
