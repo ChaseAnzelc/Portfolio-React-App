d5ce53af60c8d339b0f5835f0deb9d30d2d96c22
ChaseAnzelc <cjanzelc@gmail.com>
2025-05-19 14:14:08 -0400

    Upgrade to React 18 and React Router v6

--- src/App.js
+++ src/App.js
@@ -4,17 +4,16 @@ import './Responsive.css';
 import Footer from './components/layouts/Footer.js';
 import Home from './components/pages/Home/Home.js';
 import Projects from './components/pages/Projects/Projects.js';
-import { BrowserRouter, Switch, Route} from "react-router-dom";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Switch>
-          <Route exact path="/(|index.html)/" component={Home}/>
-          <Route path="/Projects/:projectname" component={Projects}/>
-        </Switch>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Projects/:projectname" element={<Projects />} />
+        </Routes>
         <Footer />
         <a href="#header" className="back-to-top"><i className="icofont-simple-up"></i></a>
       </BrowserRouter>
